667fcf9b3f443925891f9db6b06a7f864545a534
vladthesav <vladthesav>
2022-10-05 19:10:11 -0400

    added line to get container IP

--- server.py
+++ server.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from requests import get
+
+#get container IP
+ip = get('https://api.ipify.org').text
 
 app = Flask(__name__)
 
 @app.route("/")
-def test(): return "it works!\n"
+def test(): return "hello from {}\n".format(ip)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port = 3500)
