b6a458a47e0c9abca1b415b91966881672083dff
vladthesav <vladthesav>
2022-10-31 09:56:41 -0400

    adding code for getting data from reddit

---
+++ ml/reddit_scraping.py
@@ -0,0 +1,56 @@
+from PIL import Image
+import requests
+from io import BytesIO
+import praw
+
+def parse_gallery(gallery, max_h=900, max_w=900):
+  #parse submission object for gallery
+  #to get list of urls 
+
+  #INPUT 
+  #gallary = submission object that points to gallary 
+
+  #OPUTPUT
+  #urls = list of image urls 
+
+  #get post metadata
+  metadata = gallery.media_metadata
+  
+  #get gallary pages that look ok
+  pages = [metadata[page]["p"] for page in metadata if metadata[page]["status"]=="valid" and metadata[page]["e"]=="Image"]
+
+  #for each page, get image which isn't too big 
+  urls = []
+  #each of these has a list of the same image in differetn sizes
+  #since they are sorted, we will grab the largest image that fits within out bounds
+  for img_sizes in pages:
+    url =None
+    for img in img_sizes:
+      if img["x"] <= max_w and img["y"] <= max_h: url = img["u"]
+    if url!=None: urls.append(url)
+  return urls
+
+
+
+def get_img_urls_from_subreddit(subreddit, client=None, limit=50000):
+  #get list of image urls from subreddit
+
+  #get submissions
+  subreddit = client.subreddit(subreddit).top("all",limit=limit)
+  
+  #get submission urls 
+  submissions = [s for s in subreddit if "i.redd.it" in s.url or "/gallery/" in s.url]
+
+  img_urls = []
+
+  for submission in submissions:
+    url=submission.url
+    if "i.redd.it" in url:
+      img_urls.append(url)
+    elif "/gallery/" in url:
+      for u in parse_gallery(submission):
+        img_urls.append(u)
+
+  return img_urls
+
+
