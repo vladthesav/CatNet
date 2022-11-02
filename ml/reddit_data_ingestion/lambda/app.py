727fbde5f37753ed336fa0e1f2fc211888fdcc16
vladthesav <vladthesav>
2022-11-02 16:34:44 -0400

    testing botto client

--- ml/reddit_data_ingestion/lambda/app.py
+++ ml/reddit_data_ingestion/lambda/app.py
@@ -1,5 +1,13 @@
+import boto3
+from botocore.client import Config
+from botocore.exceptions import ClientError
+
 import sys
+import os 
+
 from reddit_data_ingestion import * 
+
+s3 = boto3.client('s3')
 
 
 def handler(event, context):
@@ -13,9 +21,16 @@ def handler(event, context):
     #make praw client
     reddit_read_only = praw.Reddit(client_id=client_id, client_secret=client_secret,user_agent = "idk")
 
-    #testing api 
-    s=get_img_urls_from_subreddit("mainecoons", client=reddit_read_only, limit=33) 
+    #get other params
+    limit_per_subreddit = event["limit"] if "limit" in event else 50 
+
+    #testing reddit api 
+    s=get_img_urls_from_subreddit("mainecoons", client=reddit_read_only, limit=limit_per_subreddit) 
     print(s)
+
+    #upload data to s3 
+    s3.put_object(Body="testing testing 123", Bucket='cat-breed-data', Key='test.txt')
+
     
 
     return 'Hello from AWS Lambda using Python' + sys.version + '!'        
