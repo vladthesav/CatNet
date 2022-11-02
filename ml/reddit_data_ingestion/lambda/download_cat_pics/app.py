06d04f8bed50131b58c76403d08fcbca75ff33c1
vladthesav <vladthesav>
2022-11-02 18:03:18 -0400

    download files to /tmp since lambda's file system is read only

--- ml/reddit_data_ingestion/lambda/download_cat_pics/app.py
+++ ml/reddit_data_ingestion/lambda/download_cat_pics/app.py
@@ -16,8 +16,8 @@ def handler(event, context):
     cat_pic_urls = event["cat_pic_urls"]
 
     # Dget list of cat pic urls
-    s3.download_file('cat-breed-data',cat_pic_urls, 'cat_pics.txt')
-    cat_pics = open('cat_pics.txt').read()
+    s3.download_file('cat-breed-data',cat_pic_urls, '/tmp/cat_pics.txt')
+    cat_pics = open('/tmp/cat_pics.txt.txt').read()
 
     
 
