a14cb4827bfa30890fa3f7883b5dec0ea496a741
= <vladyslav.o.kuz@gmail.com>
2023-05-22 21:45:17 +0000

    trying to get ec2 startup script working

---
+++ database/terraform/main.tf
@@ -0,0 +1,59 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+
+resource "aws_dynamodb_table" "cat_pic_images" {
+  name           = "CatPicMetadata"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "UserId"
+  range_key      = "GameTitle"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  attribute {
+    name = "GameTitle"
+    type = "S"
+  }
+
+  attribute {
+    name = "TopScore"
+    type = "N"
+  }
+
+  ttl {
+    attribute_name = "TimeToExist"
+    enabled        = false
+  }
+
+  global_secondary_index {
+    name               = "GameTitleIndex"
+    hash_key           = "GameTitle"
+    range_key          = "TopScore"
+    write_capacity     = 10
+    read_capacity      = 10
+    projection_type    = "INCLUDE"
+    non_key_attributes = ["UserId"]
+  }
+
+  tags = {
+    Name        = "dynamodb-table-1"
+    Environment = "production"
+  }
+}
--- ml/airflow/terraform/airflow_startup.sh
+++ ml/airflow/terraform/airflow_startup.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 yum update -y
 yum install python3-pip -y
+
+#install docker
 yum install docker -y 
 service docker restart
 
-pip -V > pip_v
-pip install apache-airflow > wtf
-airflow > plz_work 
+#install docker compose
+#curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+#sudo chmod +x /usr/local/bin/docker-compose
+#docker-compose version
 
+
+
+#get apache airflow container and run it
+#ty https://stackoverflow.com/questions/63658263/how-do-i-deploy-my-airflow-scheduler-to-aws-ec2
+docker pull puckel/docker-airflow
+#docker run -d -p 8080:8080 puckel/docker-airflow webserver
+docker run -d -p 8080:8080 -v dags:/usr/local/airflow/dags  puckel/docker-airflow webserver
